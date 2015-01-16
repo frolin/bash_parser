a123eaef1522857228414190bb26b65356fc7cdc
frolin <fralunia@gmail.com>
2015-01-16 18:40:10 +0300

    one big method sliced for to smaller

--- parse.rb
+++ parse.rb
@@ -3,29 +3,29 @@ require 'mechanize'
 require 'awesome_print'
 
 
-def parse
-
-  posts = []
-
+def goto(url)
   agent = Mechanize.new #{|a| a.log = Logger.new(STDERR) }
   agent.user_agent_alias =  'Linux Mozilla'
+  agent.get(url)
+end
 
-  page = agent.get('http://bash.im/')
+
+def parsing(page)
+  posts = []
 
   parse = page.search('#body > div.quote')
-
   parse.each { |quote|
-
     post = {
         :id => quote.search('a.id').text,
         :date => quote.search('span.date').text,
         :rating => quote.search('span.rating-o').text,
         :text => quote.search('div.text').text
     }
-
     posts << post
   }
 
-  ap posts
-
+  # posts.each { |post| puts post.to_json }
+  puts posts.to_json
+  puts "All: #{posts.count}"
 end
+
